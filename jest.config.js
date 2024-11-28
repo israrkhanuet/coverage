5ae1cb7fa0fc85da458c6ebfd31e90fdbdbc7efb
israrkhanuet <israrkhanuet@gmail.com>
2024-11-28 12:45:25 +0500

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules", "/dist"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+      isolatedModules: true,
+    },
+  },
+  globalSetup: "./jest.init.js",
+  roots: ["src/"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@app/(.*)": "<rootDir>/src/$1",
+  },
+};
